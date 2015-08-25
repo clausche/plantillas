d64eb73a50e2a453fb7b7a54f60073c10a32af5a
clausche <clausche@gmail.com>
2015-08-25 14:26:18 -0430

    hola

---
+++ system/language/english/form_validation_lang.php
@@ -0,0 +1,29 @@
+<?php
+
+$lang['required']			= "Este %s campo obligatorio.";
+$lang['isset']				= "Este Numero campo is required %s campo debe have a value.";
+$lang['valid_email']		= "Este Numero campo is required %s campo debe contain a valid email address.";
+$lang['valid_emails']		= "Este Numero campo is required %s campo debe contain all valid email addresses.";
+$lang['valid_url']			= "Este Numero campo is required %s campo debe contain a valid URL.";
+$lang['valid_ip']			= "Este Numero campo is required %s campo debe contain a valid IP.";
+$lang['min_length']			= "Este %s campo debe be at least %s characters in length.";
+$lang['max_length']			= "Este %s campo can not exceed %s characters in length.";
+$lang['exact_length']		= "Este %s campo debe debe ser exacto %s characters in length.";
+$lang['alpha']				= "Este %s campo debe contener solo alphabetical characters.";
+$lang['alpha_numeric']		= "Este %s campo debe contener solo alpha-numeric characters.";
+$lang['alpha_dash']			= "Este %s campo debe contener solo alpha-numeric characters, underscores, and dashes.";
+$lang['numeric']			= "Este %s campo debe contain only numbers.";
+$lang['is_numeric']			= "Este %s campo debe contain only numeric characters.";
+$lang['integer']			= "Este %s campo debe contain an integer.";
+$lang['regex_match']		= "Este %s campo formato incorrecto.";
+$lang['matches']			= "Este %s campo does not match Este %s campo.";
+$lang['is_unique'] 			= "Este %s campo debe contain a unique value.";
+$lang['is_natural']			= "Este %s campo debe contain only positive numbers.";
+$lang['is_natural_no_zero']	= "Este %s campo debe contain a number greater than zero.";
+$lang['decimal']			= "Este %s campo debe contain a decimal number.";
+$lang['less_than']			= "Este %s campo debe contain a number less than %s.";
+$lang['greater_than']		= "Este %s campo debe contain a number greater than %s.";
+
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/english/form_validation_lang.php */
